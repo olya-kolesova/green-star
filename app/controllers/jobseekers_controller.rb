2a8be6b4cb58c0370c03ce3f55d5fdc10efdc6a7
Olga Kolesova <olka.kolesova@gmail.com>
2021-10-27 15:11:20 +0100

    improved search and jobseeker form

--- app/controllers/jobseekers_controller.rb
+++ app/controllers/jobseekers_controller.rb
@@ -1,6 +1,10 @@
 class JobseekersController < ApplicationController
   def index
-    @jobseekers = policy_scope(Jobseeker).order(created_at: :desc)
+    if params[:query].present?
+      @jobseekers = policy_scope(Jobseeker).search_by_trade_and_last_name_and_address_line3_and_post_code(params[:query])
+    else
+      @jobseekers = policy_scope(Jobseeker).order(created_at: :desc)
+    end
   end
 
   def new
@@ -40,6 +44,6 @@ class JobseekersController < ApplicationController
   end
 
   def jobseeker_params
-    params.require(:jobseeker).permit(:first_name, :last_name, :address_line1, :address_line2, :post_code, :phone_number, :email, :trade, :qualifications, :references, :enquiry)
+    params.require(:jobseeker).permit(:first_name, :last_name, :address_line1, :address_line2, :address_line3, :post_code, :phone_number, :email, :trade, :qualifications, :references, :enquiry)
   end
 end
